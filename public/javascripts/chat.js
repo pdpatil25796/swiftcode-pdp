7ab79e669b5fd8e7a873c9b42881c77f9627452a
Live session user <pannu.bjp@gmail.com>
2017-09-16 12:15:57 +0000

    pushingto heroku

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,4 +1,9 @@
-var app = angular.module('chatApp',['ngMaterial']);
+var app = angular.module('chatApp',['ngMaterial']);.
+app.config(function($mdThemingProvider) {
+  $mdThemingProvider.theme('default')
+    .dark();
+});
+
 
 app.controller('chatController',function($scope, $sce){
 
